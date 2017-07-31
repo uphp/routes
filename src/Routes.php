dbec9ce5e1740a63314e871cbe9cb5cbd0eaf8d0
Diego Bentes <diegopbentes@gmail.com>
2017-07-31 19:17:39 -0300

    Init dev for special routes

--- src/Routes.php
+++ src/Routes.php
@@ -6,24 +6,36 @@ use UPhp\ActionDispach\Exception\NoRouteException;
 class Routes
 {
     private static $routes = [];
+    private static $specialRoutes = [];
 
-    public static function route(array $arrRoute)
+    public static function route(array $arrRoute, bool $special = false)
     {
         foreach ($arrRoute as $arr) {
             foreach ($arr as $arrOpt) {
                 if (is_array($arrOpt)) {
-                    array_push(self::$routes, $arrOpt);
+                    if (empty($special))
+                        array_push(self::$routes, $arrOpt);
+                    else
+                        array_push(self::$specialRoutes, $arrOpt);
                 } else {
-                    array_push(self::$routes, $arr);
+                    if (empty($special))
+                        array_push(self::$routes, $arr);
+                    else
+                        array_push(self::$specialRoutes, $arr);
                     break;
                 }
             }
         }
     }
 
-    public static function getControllerAction($config)
+    public static function getControllerAction($config, bool $special = false)
     {
-        foreach (self::$routes as $route ) {
+        if (empty($special))
+            $routes = self::$routes;
+        else
+            $routes = self::$specialRoutes;
+
+        foreach ($routes as $route ) {
             if ( $route["URL"] == $config["URI"] && $route["VERB"] == $config["METHOD"]) {
                 return $route;
             }
